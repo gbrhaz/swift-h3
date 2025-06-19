250b248c779487adef11c6c773cb02a5c018563b
Jer <58816840+JeremyEspresso@users.noreply.github.com>
2025-06-19 20:38:06 +0900

    update wrapper test

--- Sources/H3Kit/H3Kit.swift
+++ Sources/H3Kit/H3Kit.swift
@@ -4,20 +4,12 @@ import CH3
 
 extension CLLocationCoordinate2D {
     // Find the H3 cell index for a given set of 2D coordinates
-    public func h3CellIndex( resolution: Int32 ) -> UInt64 {
-        let lat = degsToRads(latitude)
-        let lon = degsToRads(longitude)
-        var location = GeoCoord(lat: lat, lon: lon)
-        let index = geoToH3(&location, resolution)
-        return index
-    }
-    // Find the neighbor cells for a given set of 2D coordinates and ring size
-    public func h3Neighbors( resolution: Int32, ringLevel: Int32 ) -> [H3Index]
-    {
-        let index = h3CellIndex(resolution: resolution)
-        let count = Int(maxKringSize(ringLevel))
-        var neighbors = Array(repeating: H3Index(), count: count)
-        kRing(index, ringLevel, &neighbors);
-        return neighbors
+    public func h3CellIndex( resolution: Int32 ) -> H3Index? {
+        var latLng = LatLng(lat: lat, lon: lng)
+        var h3index: H3Index = 0
+
+        let error = latLngToCell(&latLng, resolution, &h3index)
+
+        return error == 0 ? h3index : nil
     }
 }
